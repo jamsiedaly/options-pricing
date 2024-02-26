3c21d8090b8a5bf109ec86a0186401af462777e8
James Daly <jamsiedaly@gmail.com>
2024-02-26 22:25:50 +0100

    Created an implied interest rate function

--- src/main.rs
+++ src/main.rs
@@ -20,3 +20,19 @@ fn calculate_forward_price(
     let future_price = future_cash_value - expected_dividend.0;
     return ForwardPrice(future_price);
 }
+
+fn calculate_implied_interest(
+    forward_price: ForwardPrice,
+    stock_price: StockPrice,
+    time_to_maturity: TimeToMaturity,
+    expected_dividend: DividendYield
+) -> InterestRate {
+    let future_earnings = forward_price.0 + expected_dividend.0;
+    let margin = (future_earnings / stock_price.0) - 1.0;
+    let interest_rate = margin / time_to_maturity.0;
+    return InterestRate(interest_rate);
+}
+
+#[cfg(test)]
+mod tests {
+}
